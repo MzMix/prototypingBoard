9ee20335438e9ca022a63bd6e958adcffcd7d45f
Mikołaj Zuziak <m.w.zuziak@gmail.com>
2024-08-28 17:06:14 +0200

    deploy

--- src/utils/utils.js
+++
@@ -1,8 +0,0 @@
-export function getFontColorForBackfround(color) {
-    let r = parseInt(color.substr(1, 2), 16);
-    let g = parseInt(color.substr(3, 2), 16);
-    let b = parseInt(color.substr(5, 2), 16);
-    let yiq = ((r * 299) + (g * 587) + (b * 114)) / 1000;
-
-    return (yiq >= 128) ? '#101' : '#fff';
-}
